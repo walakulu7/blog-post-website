d3fb51a14bec80b9fdb07465ca29f37289b0b5b7
Janaka Walakuluarachchi <walakulu@gmail.com>
2023-03-03 17:52:21 +0530

    install lodash

--- app.js
+++ app.js
@@ -44,6 +44,22 @@ app.post("/compose", function(req, res) {
   res.redirect("/");
 });
 
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function(post) {
+    const storedTitle = post.title;
+
+    if (storedTitle === requestedTitle) {
+      console.log("Match Found!");
+    } else {
+      console.log("Not a match!");
+    }
+
+  });
+  
+});
+
 
 
 
