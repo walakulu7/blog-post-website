1de702cf44dba390522f9ffd322ecccf0767e4ff
Janaka Walakuluarachchi <walakulu@gmail.com>
2023-03-03 11:25:36 +0530

    ejs file added and code written

--- app.js
+++ app.js
@@ -13,15 +13,36 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
+let posts = [];
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent, 
+    newPosts: posts});
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutPgContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactPgContent: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
-
-
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
